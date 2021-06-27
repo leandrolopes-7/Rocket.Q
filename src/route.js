f5f223b3e18359304635af7fab9cb5e3d9c24796
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-27 13:10:55 -0300

    RocketQ

--- src/route.js
+++ src/route.js
@@ -5,6 +5,8 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
-route.post('/room/:room/:question/:action', (req, res) => res.render())
+
+//Formato que o formulario de dentro da modal tem que passar a informação
+//route.post('/roo/:room/:question/:action',)
 
 module.exports = route
