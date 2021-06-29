66943831cafe987e260e553c708b15952dd1a1ea
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-28 21:58:07 -0300

    RocketQ

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,19 +5,46 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
+        while (isRoom) {
 
-        for (let i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-                roomId += Math.floor(Math.random() * 10).toString()
+            for (let i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                    roomId += Math.floor(Math.random() * 10).toString()
+            }
+            const roomExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomExistIds.some(roomExistIds => roomExistIds === roomId)
+
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                        id, pass
+                    ) VALUES (
+                        ${parseInt(roomId)}, ${pass}
+                    )`)
+            }
         }
-
-        await db.run(`INSERT INTO rooms (
-            id, pass
-        ) VALUES (
-            ${parseInt(roomId)}, ${pass}
-        )`)
-
         db.close()
+
+        res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res) {
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions = true
+
+        if (questions.length == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = false
+            }
+        }
+        res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions })
+    },
+
+    enter(req, res) {
+        const roomId = req.body.roomId
 
         res.redirect(`/room/${roomId}`)
     }
